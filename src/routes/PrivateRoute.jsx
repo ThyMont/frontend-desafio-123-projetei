105e839d48d6d588109fd6f9e1ebb7c6fb62fb03
Thyago Monteiro <tmacademico@gmail.com>
2021-06-22 17:53:05 -0300

    Update PrivateRoute.jsx

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -4,14 +4,6 @@ import { useSelector } from 'react-redux';
 export default function PrivateRoute({ component, ...props }) {
   const loggedIn = useSelector((state) => state.user);
 
-  //const loggedIn = !!localStorage.getItem('app-token');
-
-  // return (
-  //   <Route
-  //     {...props}
-  //     render={() => (loggedIn ? component : <Redirect from="*" to="/login" />)}
-  //   ></Route>
-  // );
   return loggedIn ? (
     <Route {...props} component={component} />
   ) : (
