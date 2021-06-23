1abc38d5c57ebecdabc6e8af703c1771ddd324fb
Thyago Monteiro <tmacademico@gmail.com>
2021-06-22 22:24:26 -0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import userReducer from './User/User.reducer';
 import pessoaFisicaReducer from './PessoaFisica/PessoaFisica.Reducer';
 import storeSynchronize from 'redux-localstore';
+import thunk from 'redux-thunk';
 
 const rootReducer = combineReducers({
   user: userReducer,
   pessoaFisica: pessoaFisicaReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, {}, applyMiddleware(thunk));
 
 export default store;
 storeSynchronize(store);
